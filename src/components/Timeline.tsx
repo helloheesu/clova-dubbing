f3fbbba5640cc9e78906c7622fcf37e50a2a4954
Heesu Jung (heesu-jung) <heesu.jung@odkmedia.net>
2021-03-23 22:34:29 +0900

    ✨ CurrentPositionIndicator

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./Timeline.scss";
 
 type millisecond = number;
-interface props {
+interface timelineProps {
   length: millisecond;
   scale: millisecond;
+}
+
+interface timeIndicatorProps {
+  time: millisecond;
 }
 
 const localeMs = (ms: millisecond) => {
@@ -24,19 +28,28 @@ const localeMs = (ms: millisecond) => {
   return `${minute}:${second}.${millisecond}`;
 };
 
-const TimeStep = ({ time }: { time: millisecond }) => (
+const TimeStep = ({ time }: timeIndicatorProps) => (
   <div className="timestep">
     <span>{localeMs(time)}</span>
   </div>
 );
 
-const Timeline = ({ length, scale }: props) => {
+const CurrentPositionIndicator = ({ time }: timeIndicatorProps) => {
+  return <div className="position-indicator">{time}</div>;
+};
+
+const Timeline = ({ length, scale }: timelineProps) => {
   const timesteps = Array.apply(
     null,
     Array(Math.floor(length / scale) + 1)
   ).map((_, i) => <TimeStep key={i * scale} time={i * scale} />);
 
-  return <div className="timeline">{timesteps}</div>;
+  return (
+    <div className="timeline">
+      <CurrentPositionIndicator time={0} />
+      {timesteps}
+    </div>
+  );
 };
 
 export default Timeline;
