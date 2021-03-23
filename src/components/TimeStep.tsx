eefe02e08a463e1254aada75b3bec7e75743be56
Heesu Jung (heesu-jung) <heesu.jung@odkmedia.net>
2021-03-24 00:41:35 +0900

    🚚 extract time util

--- src/components/TimeStep.tsx
+++ src/components/TimeStep.tsx
@@ -1,22 +1,5 @@
 import React from "react";
-
-const localeMs = (ms: millisecond) => {
-  const SECOND_TO_MS = 1000;
-  const MINUTE_TO_MS = 60 * SECOND_TO_MS;
-
-  const minute = Math.floor(ms / MINUTE_TO_MS)
-    .toString()
-    .padStart(2, "0");
-  const second = Math.floor((ms % MINUTE_TO_MS) / SECOND_TO_MS)
-    .toString()
-    .padStart(2, "0");
-  const millisecond = Math.floor(ms % SECOND_TO_MS)
-    .toString()
-    .padStart(2, "0");
-
-  return `${minute}:${second}.${millisecond}`;
-};
-
+import { localeMs } from "../utils/time";
 interface props {
   time: millisecond;
 }
