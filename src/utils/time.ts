3b197b00a41ccdfadf304fd13968e8262b8e337f
Heesu Jung (heesu-jung) <heesu.jung@odkmedia.net>
2021-03-24 00:49:56 +0900

    🐛 time locale fix point

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -5,12 +5,10 @@ export const localeMs = (ms: millisecond) => {
   const minute = Math.floor(ms / MINUTE_TO_MS)
     .toString()
     .padStart(2, "0");
-  const second = Math.floor((ms % MINUTE_TO_MS) / SECOND_TO_MS)
+  const second = ((ms % MINUTE_TO_MS) / SECOND_TO_MS)
+    .toFixed(2)
     .toString()
-    .padStart(2, "0");
-  const millisecond = Math.floor(ms % SECOND_TO_MS)
-    .toString()
-    .padStart(2, "0");
+    .padStart(5, "0");
 
-  return `${minute}:${second}.${millisecond}`;
+  return `${minute}:${second}`;
 };
