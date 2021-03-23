34132deb4568b341c56a8dec469a398ef02af588
Heesu Jung (heesu-jung) <heesu.jung@odkmedia.net>
2021-03-23 20:00:41 +0900

    ✨ Timestep

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,11 +7,36 @@ interface props {
   scale: millisecond;
 }
 
-const Timeline = ({ length, scale }: props) => {
-  return (
-    <div className="timeline">
-      {length}, {scale}
-    </div>
-  );
+const localeMs = (ms: millisecond) => {
+  const SECOND_TO_MS = 1000;
+  const MINUTE_TO_MS = 60 * SECOND_TO_MS;
+
+  const minute = Math.floor(ms / MINUTE_TO_MS)
+    .toString()
+    .padStart(2, "0");
+  const second = Math.floor((ms % MINUTE_TO_MS) / SECOND_TO_MS)
+    .toString()
+    .padStart(2, "0");
+  const millisecond = Math.floor(ms % SECOND_TO_MS)
+    .toString()
+    .padStart(2, "0");
+
+  return `${minute}:${second}.${millisecond}`;
 };
+
+const TimeStep = ({ time }: { time: millisecond }) => (
+  <div className="timestep">
+    <span>{localeMs(time)}</span>
+  </div>
+);
+
+const Timeline = ({ length, scale }: props) => {
+  const timesteps = Array.apply(
+    null,
+    Array(Math.floor(length / scale) + 1)
+  ).map((_, i) => <TimeStep key={i * scale} time={i * scale} />);
+
+  return <div className="timeline">{timesteps}</div>;
+};
+
 export default Timeline;
