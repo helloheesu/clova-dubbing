7e9fe9f30fd33e929606026e761305b0539be10d
Heesu Jung (heesu-jung) <heesu.jung@odkmedia.net>
2021-03-26 18:34:05 +0900

    ✨ move AudioBox by ArrowKey

--- src/components/AudioTimeline.tsx
+++ src/components/AudioTimeline.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import { audioBoxsState, stepInfoState } from "../recoil/atoms";
 import { localeMs, timeToPosition } from "../utils/time";
 
@@ -7,7 +7,7 @@ const AudioTimeline = () => {
   const { duration: stepDuration, width: stepWidth } = useRecoilValue(
     stepInfoState
   );
-  const audioBoxs = useRecoilValue(audioBoxsState);
+  const [audioBoxs, setAudioBoxs] = useRecoilState(audioBoxsState);
 
   return (
     <ul
@@ -19,6 +19,31 @@ const AudioTimeline = () => {
           <li
             key={`${startAt}-${src.url}`}
             className="audio-box"
+            tabIndex={1}
+            onKeyDown={(e) => {
+              if (e.code !== "ArrowLeft" && e.code !== "ArrowRight") {
+                return;
+              }
+
+              e.preventDefault();
+
+              setAudioBoxs((audioBoxs) => {
+                const findingUrl = src.url;
+                const audioBoxIndex = audioBoxs.findIndex(
+                  ({ src }) => src.url === findingUrl
+                );
+
+                return [
+                  ...audioBoxs.slice(0, audioBoxIndex),
+                  {
+                    src,
+                    startAt:
+                      e.code === "ArrowRight" ? startAt + 100 : startAt - 100,
+                  },
+                  ...audioBoxs.slice(audioBoxIndex + 1),
+                ];
+              });
+            }}
             style={{
               width: timeToPosition(src.duration, stepDuration, stepWidth),
               left: `${timeToPosition(startAt, stepDuration, stepWidth)}px`,
